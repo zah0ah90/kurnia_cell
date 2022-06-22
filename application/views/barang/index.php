982f93279c9e73a3f2a0c2488eec335c3f04dd0a
zah0ah <zah0ah@gmail.com>
2022-06-22 12:48:57 +0700

    perbaikan query

--- application/views/barang/index.php
+++ application/views/barang/index.php
@@ -15,7 +15,7 @@
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
     <div class="card-body">
-        <a href="<?= base_url('barang/tambah') ?>" class="btn btn-primary float-right mb-2">Tambah</a>
+        <a href="<?= base_url('barang/tambah') ?>" class="btn btn-primary float-right mb-5">Tambah</a>
         <div class="table-responsive">
             <table class="table table-bordered" id="example" width="100%" cellspacing="0">
                 <thead>
@@ -32,30 +32,28 @@
                 <tbody>
                     <?php $no = 1;
                     foreach ($tabel as $data) { ?>
-                    <tr>
-                        <td><?= $no++ ?></td>
-                        <td><?= $data->nama_barang ?></td>
-                        <td><?= $data->nama_supplier ?></td>
-                        <td>
-                            <?php if ($data->stok == 0) {
+                        <tr>
+                            <td><?= $no++ ?></td>
+                            <td><?= $data->nama_barang ?></td>
+                            <td><?= $data->nama_supplier ?></td>
+                            <td>
+                                <?php if ($data->stok == 0) {
                                     $stok = 0;
                                 } else {
                                     $stok = $data->stok;
                                 } ?>
-                            <?= $stok ?>
-                        </td>
-                        <td><?= uang($data->harga_jual) ?></td>
-                        <td><?= uang($data->harga_modal) ?></td>
-                        <td>
-                            <a href="<?= base_url('barang/edit/' . $data->id) ?>"><span
-                                    class="btn btn-primary">Edit</span></a>
-                            <a href="<?= base_url('barang/tambah_stok/' . $data->id) ?>" class="btn btn-success">+ /
-                                -</a>
-                            <a href="<?= base_url('barang/hapus/' . $data->id) ?>"
-                                class="btn btn-danger tombol-hapus">Hapus</a>
+                                <?= $stok ?>
+                            </td>
+                            <td><?= uang($data->harga_jual) ?></td>
+                            <td><?= uang($data->harga_modal) ?></td>
+                            <td>
+                                <a href="<?= base_url('barang/edit/' . $data->id) ?>"><span class="btn btn-primary">Edit</span></a>
+                                <a href="<?= base_url('barang/tambah_stok/' . $data->id) ?>" class="btn btn-success">+ /
+                                    -</a>
+                                <a href="<?= base_url('barang/hapus/' . $data->id) ?>" class="btn btn-danger tombol-hapus">Hapus</a>
 
-                        </td>
-                    </tr>
+                            </td>
+                        </tr>
                     <?php } ?>
                 </tbody>
 
@@ -65,38 +63,38 @@
 </div>
 
 <script>
-$(document).ready(function() {
-    $('#example').DataTable();
-    $('.tombol-hapus').on('click', function(e) {
-        e.preventDefault();
+    $(document).ready(function() {
+        $('#example').DataTable();
+        $('.tombol-hapus').on('click', function(e) {
+            e.preventDefault();
 
-        const href = $(this).attr('href');
+            const href = $(this).attr('href');
 
-        Swal.fire({
-            title: 'Apakah anda yakin?',
-            text: "Data Obat akan dihapus!",
-            type: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Hapus Data!'
-        }).then((result) => {
-            if (result.value == true) {
-                document.location.href = href;
-            }
+            Swal.fire({
+                title: 'Apakah anda yakin?',
+                text: "Data Obat akan dihapus!",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Hapus Data!'
+            }).then((result) => {
+                if (result.value == true) {
+                    document.location.href = href;
+                }
+            })
         })
-    })
 
-    const flashData = $('.flash-data').data('flashdata');
-    if (flashData) {
-        Swal.fire({
-            title: 'Data Spare Part',
-            text: flashData,
-            type: 'success'
-        })
-    }
+        const flashData = $('.flash-data').data('flashdata');
+        if (flashData) {
+            Swal.fire({
+                title: 'Data Spare Part',
+                text: flashData,
+                type: 'success'
+            })
+        }
 
-});
+    });
 </script>
 <!-- Page level plugins -->
 
